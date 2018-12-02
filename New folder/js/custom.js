baea8f83002e8bc17e6f366fd9b877d7700f7f35
patelmit15 <patelmit15@gmail.com>
2018-12-02 16:21:09 +1000

    changes in logo and hamburger

---
+++ New folder/js/custom.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+
+
+$(document).ready(function(){
+	$('.hamIcon').click(function(){
+		$('.hamIcon').toggleClass('hamActive');
+	});
+});
+
+/*
+
+function toggleSidebar(){
+	document.getElementsByClassName("left-sidebar")[0].classList.toggle('sideBarActive');
+}
+
+*/
