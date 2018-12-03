6dec3058b6c41ee66f239ed265f0f0b79e52602e
patelmit15 <patelmit15@gmail.com>
2018-12-03 11:59:09 +1000

    header with search

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,14 @@
 // JavaScript Document
 
-
+/* Hamburger */
 
 $(document).ready(function(){
 	$('.hamIcon').click(function(){
 		$('.hamIcon').toggleClass('hamActive');
 	});
 });
+
+/* Sidebar */
 
 /*
 
@@ -15,3 +17,38 @@ function toggleSidebar(){
 }
 
 */
+
+/* Search Menu*/
+
+$(document).ready(function(){
+		$('#search').click(function(){
+			$('.menu-item').addClass('hide-item');
+			$('.search-form').addClass('active');
+			$('.close').addClass('active');
+//			$('#search').hide();			
+			function searchFocus() {
+			$('#search-bar').focus();
+			}		
+			setTimeout(searchFocus, 1000);
+		});
+		
+	
+ 		$('.close').click(function(){
+			$('.menu-item').removeClass('hide-item');
+			$('.search-form').removeClass('active');
+			$('.close').removeClass('active');
+			$('#search').show();
+		});
+});
+
+/* Fixed header */
+
+$(window).on('scroll',function(){
+	if ($(window).scrollTop()){
+		$('header').addClass('scrolled-header');
+	}
+	else {
+		$('header').removeClass('scrolled-header');
+	}
+});
+
