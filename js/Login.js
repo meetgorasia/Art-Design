555b5f948edffba5ccf99aab0931c7073d89d851
Rutvik3131 <34470994+Rutvik3131@users.noreply.github.com>
2018-12-10 13:05:54 +1000

    login
    updated

---
+++ js/Login.js
@@ -0,0 +1,14 @@
+
+
+$('.message a').click(function(){
+	$('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+
+ 
+ $(document).ready(function(){
+    $("#login").click(function(){
+        $("#logform").toggle();
+        if (e.srcElement !== div) $(div).hide();
+    });
+});
+
