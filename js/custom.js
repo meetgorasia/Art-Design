7e813ed644f7128a921231c55ed351de80ee31e8
patelmit15 <patelmit15@gmail.com>
2018-12-14 13:35:32 +1000

    master design

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,50 @@
 // JavaScript Document
 
+//Closing Popups
+
+//  --  Closing Sidebar
+
+$(document).on('click touch', function(event) {
+  if (!$(event.target).parents().addBack().is('#hamIcon')) {
+    $('#sidebar').removeClass('sidebarActive');
+	$('#hamIcon').removeClass('hamActive');
+  }
+});
+ 
+$('#sidebar').on('click touch', function(event) {
+  event.stopPropagation();
+});
+
+//  --  Closing Logform
+
+$(document).on('click touch', function(event) {
+  if (!$(event.target).parents().addBack().is('#login')) {
+    $('#logform').removeClass('logformActive');
+  }
+});
+ 
+$('#logform').on('click touch', function(event) {
+  event.stopPropagation();
+});
+
+
+//  --  Search bar
+
+$(document).on('click touch', function(event) {
+  if (!$(event.target).parents().addBack().is('#search')) {
+    $('.menu-item').removeClass('hide-item');
+	$('.search-form').removeClass('active');
+	$('.close').removeClass('active');
+	$('#search').show();
+  }
+});
+ 
+$('.search-form').on('click touch', function(event) {
+  event.stopPropagation();
+});
+
+
+
 /* Hamburger */
 
 $(document).ready(function(){
@@ -9,7 +54,6 @@ $(document).ready(function(){
 });
 
 /* Sidebar */
-
 
 function toggleSidebar(){
 	document.getElementsByClassName("sidebar")[0].classList.toggle('sidebarActive');
@@ -39,14 +83,46 @@ $(document).ready(function(){
 		});
 });
 
-/* Fixed header */
+/* Scrolled header */
 
 $(window).on('scroll',function(){
 	if ($(window).scrollTop()){
 		$('header').addClass('scrolled-header');
+//		$('.home-hero').slideUp(2000);
 	}
 	else {
 		$('header').removeClass('scrolled-header');
+//		$('.home-hero').slideDown(2000);
 	}
 });
 
+/* Parallex */
+
+$(window).scroll(function(){
+	parallax();
+});
+
+function parallax(){
+	var wScroll = $(window).scrollTop();
+	
+	$('.parallax--bg').css('background-position', 'center '+(wScroll*0.75)+'px');
+}
+
+//Log in form
+
+$('.message a').click(function(){
+	$('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+  
+// $(document).ready(function(){
+//    $("#login").click(function(){
+//        $("#logform").toggle();
+//        if (e.srcElement !== div) $(div).hide();
+//    });
+//});
+
+$(document).ready(function(){
+	$('#login').click(function(){
+		$('#logform').toggleClass('logformActive');
+	});
+});
